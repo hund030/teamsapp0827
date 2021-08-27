1433b34c96105381548144459ab8f174dce26016
zhijie <zhihuan@microsoft.com>
2021-08-27 13:36:10 +0800

    init

---
+++ tabs/src/components/Tab.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Welcome } from "./sample/Welcome";
+import { Button } from "@material-ui/core";
+
+export default function Tab() {
+  return <div>
+    <Button variant="contained"></Button>
+  </div>;
+}
